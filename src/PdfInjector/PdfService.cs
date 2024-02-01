d0b7350d9ec2c4838a6176b312bdc781419e38e2
rulasg <rulasg@outlook.com>
2024-02-01 06:56:14 +0100

    wip

--- src/PdfInjector/PdfService.cs
+++ src/PdfInjector/PdfService.cs
@@ -18,6 +18,9 @@ public class PdfService
             case "solidify_training_v1":
                 tempFile = InjectNameV1(docInfo, pdfPath);
                 break;
+            case "solidify_training_v2":
+                tempFile = InjectNameV2(docInfo, pdfPath);
+                break;
             default:
                 tempFile = InjectNameDefault(docInfo, pdfPath);
                 break;
@@ -168,8 +171,107 @@ public class PdfService
         return tempFile;
     }
 
+    string InjectNameV2(DocInfo docInfo, string pdfPath)
+    {
+        string tempFile = Path.GetTempFileName();
+
+        using (PdfReader reader = new PdfReader(pdfPath))
+        {
+            using (PdfStamper stamper = new PdfStamper(reader, new FileStream(tempFile, FileMode.Create)))
+            {
+                // Get the size of the page
+                // Rectangle pageSize = reader.GetPageSize(1);
+                // float middle_X = pageSize.Width / 2;
+                // float middle_y = pageSize.Height / 2;
+
+                PdfContentByte canvas = stamper.GetOverContent(1);
+                canvas.BeginText();
+
+                // Paragraph 1 Base Stamp
+                var stamp = new Stamp(
+                    "empty",
+                    new Position (297.75f,380,0),
+                    35, // Font Size
+                    BaseFont.TIMES_ITALIC,
+                    new BaseColor(64, 119, 142) // Green elegant
+                );
+
+                //Name
+                var maxLength = "012345678901234567890123456789".Length;
+                var text = docInfo.Student.Name;
+                if (text.Length > maxLength)
+                {
+                    throw new ArgumentException($"Student name is longer than {maxLength} characters");
+                }
+
+                stamp.Text = text;
+                StampText(canvas, stamp);
+
+                //Handle and Company
+                stamp.Text = string.IsNullOrWhiteSpace(docInfo.Student.Handle) ? string.Empty : $"@{docInfo.Student.Handle}";
+                stamp.Text += string.IsNullOrWhiteSpace(docInfo.Student.Company) ? string.Empty : $" ({docInfo.Student.Company})";
+                // stamp.Text = $"@{docInfo.Student.Handle} ({docInfo.Student.Company})";
+                stamp.FontSize -= 15;
+                stamp.Position.Y-= stamp.FontSize + 3; // Second line
+                StampText(canvas, stamp);
+
+                // Training Name
+                stamp.Color = BaseColor.DARK_GRAY;
+                stamp.Text = $"{docInfo.CourseName}";
+                stamp.Position.Y -= stamp.FontSize * 2; // Third line
+                StampText(canvas, stamp);
+
+                // Training Date
+                stamp.Text = $"{docInfo.CourseDate}";
+                stamp.FontSize -= 5;
+                stamp.Position.Y -= stamp.FontSize + 3; // Fourth line
+                StampText(canvas, stamp);
+
+                // Trainer signature SECTION
+                stamp = new Stamp(
+                    "empty",
+                    new Position (0,0,0),
+                    0, // Font Size
+                    BaseFont.TIMES_BOLDITALIC,
+                    BaseColor.BLACK
+                );
+
+                // Trainer Name
+                stamp.FontSize = 16;
+                stamp.Text = docInfo.Trainer.Name;
+                stamp.Position = new Position(421.125f, 200f, 0f);
+                StampText(canvas,stamp);
+
+                // Handle Trainer
+                stamp.Text = $"@{docInfo.Trainer.Handle}";
+                stamp.Position.Y -= stamp.FontSize; // Second line
+                stamp.FontSize = 12;
+                StampText(canvas, stamp);
+
+                // GUID
+                stamp = new Stamp(
+                    docInfo.Id.ToString(),
+                    new Position (297.75f,0,0),
+                    6, // Font Size
+                    BaseFont.COURIER,
+                    BaseColor.BLACK
+                );
+                stamp.Position.Y = 12;
+                StampText(canvas, stamp);
+
+                //Close canvas
+                canvas.EndText();
+            }
+        }
+
+        return tempFile;
+    }
+
     void StampText(PdfContentByte canvas, Stamp stamp)
     {
+        if(String.IsNullOrWhiteSpace(stamp.Text))
+            return;
+
         BaseFont bfName = BaseFont.CreateFont(stamp.FontName, BaseFont.CP1252, BaseFont.NOT_EMBEDDED);
         var color = new BaseColor(stamp.Color.R, stamp.Color.G, stamp.Color.B);
 
